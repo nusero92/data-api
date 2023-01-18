dfa5d19155b1ec27834ed684c0567fd8402dd59d
anamaria lukacs <anamarialukacs030@gmail.com>
2023-01-18 17:19:35 +0100

    partially resolved

--- weather.py
+++ weather.py
@@ -11,18 +11,32 @@ def search_city(query):
     '''Look for a given city. If multiple options are returned, have the user choose between them.
        Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    response = requests.get(f"https://weather.lewagon.com/geo/1.0/direct?q={query}&limit=5" ).json()
+    if not response:
+        return None
+    citys=response[0]
+    return {'name': citys['name'],
+            'lat':citys['lat'],
+            'lon':citys['lon'],
+            'country': citys['country'],
+            'state': citys['state'] }
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+
+    response = requests.get(f"https://weather.lewagon.com/data/2.5/forecast", params={"lat":lat,"lon":lon}).json()
+    forecast=response["list"][::8]
+    return forecast
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
     # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    question=input("Multiple matches found, which city did you mean?")
+
+
+
 
 if __name__ == '__main__':
     try:
